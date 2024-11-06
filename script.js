a79b216587c1ab96cdcbb8872523c2410b997ad4
whyura <wahyusyahputra010805@gmail.com>
2024-11-06 09:35:49 +0700

    change text

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const buttonAdd = document.getElementById("add-list");
 const list = document.getElementById("list");
 
 function addList() {
-  const text = prompt("Enter Text");
+  const text = prompt("Masukkan Teks");
   if (text) {
     const textNode = document.createTextNode(text);
     const li = document.createElement("li");
@@ -11,7 +11,7 @@ function addList() {
 
     // Buat tombol delete
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
+    deleteButton.textContent = "Hapus";
     deleteButton.style.marginLeft = "10px";
     deleteButton.addEventListener("click", () => {
       list.removeChild(li);
