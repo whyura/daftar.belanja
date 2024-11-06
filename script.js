6e407597ca95bd513b1681f54afd2cb355c1e1f6
whyura <wahyusyahputra010805@gmail.com>
2024-11-06 09:23:43 +0700

    add button delete js

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function addList() {
     const li = document.createElement("li");
     li.appendChild(textNode);
     list.appendChild(li);
+
+    // Buat tombol delete
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.addEventListener("click", () => {
+      list.removeChild(li);
+    });
+    li.appendChild(deleteButton);
+    list.appendChild(li);
   }
 }
 
